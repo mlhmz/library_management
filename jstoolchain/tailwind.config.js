6f8730efcb50334069778e69ff5e3c44212d928e
Malek Hamza <malek.hamza@gmx.de>
2023-11-10 22:40:51 +0100

    Add login to application

--- jstoolchain/tailwind.config.js
+++ jstoolchain/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["../**/templates/**/*.html"],
+    content: ["../library/templates/**/*.html"],
     theme: {
         extend: {},
     },
