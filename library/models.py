c581e82487ce612e0813ec747b10b65fb111c9bb
Malek Hamza <malek.hamza@gmx.de>
2023-11-10 18:05:14 +0100

    Improve relationships

--- library/models.py
+++ library/models.py
@@ -5,16 +5,25 @@ class Genre(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Author(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Book(models.Model):
     id = models.BigAutoField(primary_key=True)
     isbn = models.CharField(max_length=13)
     title = models.CharField(max_length=255)
     year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     genres = models.ManyToManyField(Genre)
 
-
-class Author(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.title} ({self.year})'
