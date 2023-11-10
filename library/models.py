781460eba934491a658b2b12092badeca766a7ef
Malek Hamza <malek.hamza@gmx.de>
2023-11-10 17:48:38 +0100

    Replace book uuid with big auto field and add it to other models

--- library/models.py
+++ library/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Genre(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
 
 
 class Book(models.Model):
-    id = models.UUIDField(primary_key=True, unique=True)
+    id = models.BigAutoField(primary_key=True)
     isbn = models.CharField(max_length=13)
     title = models.CharField(max_length=255)
     year = models.IntegerField()
@@ -14,5 +15,6 @@ class Book(models.Model):
 
 
 class Author(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
