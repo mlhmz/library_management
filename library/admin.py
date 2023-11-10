82387906fdc9077ee91b463f47525d6d9bb8e42e
Malek Hamza <malek.hamza@gmx.de>
2023-11-10 16:03:44 +0100

    Add admin ui

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Book, Genre, Author
+
+
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+
+
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    model = Author
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Author, AuthorAdmin)
