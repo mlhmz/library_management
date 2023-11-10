a6c2ee6f17cab15c1d20911ef5d4f610ec1e2cf8
Malek Hamza <malek.hamza@gmx.de>
2023-11-10 20:23:10 +0100

    Reduce pagination objects per page

--- library/views.py
+++ library/views.py
@@ -5,7 +5,7 @@ from .models import Book
 
 # Create your views here.
 class BookListView(ListView):
-    paginate_by = 25
+    paginate_by = 10
     model = Book
     template_name = 'book_list.html'
     context_object_name = "books"
